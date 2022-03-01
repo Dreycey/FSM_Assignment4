b6644151764cac64aebfe97782ae9bb0ddce711d
Dreycey <albindreycey@gmail.com>
2022-03-01 15:22:45 -0700

    updating main file

--- source/FSM_StopLights.c
+++ source/FSM_StopLights.c
@@ -1,72 +1,56 @@
-/*
- * Copyright 2016-2022 NXP
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * o Redistributions of source code must retain the above copyright notice, this list
- *   of conditions and the following disclaimer.
- *
- * o Redistributions in binary form must reproduce the above copyright notice, this
- *   list of conditions and the following disclaimer in the documentation and/or
- *   other materials provided with the distribution.
- *
- * o Neither the name of NXP Semiconductor, Inc. nor the names of its
- *   contributors may be used to endorse or promote products derived from this
- *   software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
- 
+
 /**
- * @file    FSM_StopLights.c
+ * @file    Assignment4_FSM_Buffahiti.c
  * @brief   Application entry point.
+ *
+ * Description:
+ *
+ *     Primary file for running stop lights
+ *     for the people of Buffahiti.
  */
 #include <stdio.h>
 #include "board.h"
-#include "peripherals.h"
-#include "pin_mux.h"
-#include "clock_config.h"
 #include "MKL25Z4.h"
 #include "fsl_debug_console.h"
-/* TODO: insert other include files here. */
+#include "UART.h"
+#include "tsi_module.h"
+#include "log.h"
+#include "LEDPWM.h"
+#include "timer.h"
+#include "stoplight_fsm.h"
 
-/* TODO: insert other definitions and declarations here. */
+
+//
+//volatile uint32_t slider_val = 0;
+
+
+//
+///*
+// *  Global value for keeping information
+// *  from the touch sensor
+// */
+//volatile uint32_t slider_val = 0;
+
+void stoplight_activate(void) {
+	while (1) {
+		operate_stoplights();
+		read_touchsensor();
+	}
+}
 
 /*
- * @brief   Application entry point.
+ * MAIN
  */
 int main(void) {
+    /* Init board hardware. */
+	BOARD_InitBootClocks();
 
-  	/* Init board hardware. */
-    BOARD_InitBootPins();
-    BOARD_InitBootClocks();
-    BOARD_InitBootPeripherals();
-#ifndef BOARD_INIT_DEBUG_CONSOLE_PERIPHERAL
-    /* Init FSL debug console. */
-    BOARD_InitDebugConsole();
-#endif
+    // Initialize UART, TSI, PWM
+	Touch_Init();
+	Init_Blue_LED_PWM(PWM_PERIOD);
+	init_systick();
 
-    PRINTF("Hello World\n");
-
-    /* Force the counter to be placed into memory. */
-    volatile static int i = 0 ;
-    /* Enter an infinite loop, just incrementing a counter. */
-    while(1) {
-        i++ ;
-        /* 'Dummy' NOP to allow source level single stepping of
-            tight while() loop */
-        __asm volatile ("nop");
-    }
-    return 0 ;
+	// run Finite State Machine
+	stoplight_activate();
+    return 0;
 }
